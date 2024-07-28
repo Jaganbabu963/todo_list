1d4ffbd73a1191d9ee01704ab3dbe2da15a5374d
Jaganbabu963 <jagancheemal@gmail.com>
2024-07-28 09:55:17 +0530

    Del Button Modify

--- src/components/Additem.jsx
+++ src/components/Additem.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import { MdOutlineDeleteForever } from "react-icons/md";
 
-function ToDoitem(props){
-
-     return <div className="cross" >
-     <li>{props.text}
-     <MdOutlineDeleteForever onClick={()=>{props.delF(props.id)}} />
-     </li>
-     
-     </div>  
+function ToDoitem(props) {
+  return (
+    <div className="cross">
+      <li>
+        {props.text}
+        <MdOutlineDeleteForever
+          onClick={() => {
+            props.delF(props.id);
+          }}
+        />
+      </li>
+    </div>
+  );
 }
 export default ToDoitem;
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,37 +1,28 @@
- import React, { useState } from "react";
+import React, { useState } from "react";
 import ToDoitem from "./Additem";
 import Upper from "./Upper";
 
 function App() {
+  let [arrTask, setArrTask] = useState([]);
 
-  
-  let [arrTask,setArrTask] = useState([]);
-
-
-
-  function handleClick(task){
-     setArrTask((x)=> [...x,task])
+  function handleClick(task) {
+    setArrTask((x) => [...x, task]);
   }
 
-  function deleteItem(id){
-    setArrTask((prev)=>{return prev.filter((e,index) => index!==id)
-    })
+  function deleteItem(id) {
+    setArrTask((prev) => {
+      return prev.filter((e, index) => index !== id);
+    });
   }
 
   return (
     <div className="container">
-      <Upper 
-      onAdd={handleClick}
-      />
+      <Upper onAdd={handleClick} />
       <div>
         <ul>
-           {arrTask.map((e,index) =>
-            <ToDoitem 
-            id = {index}
-            delF = {deleteItem}
-            text = {e}   
-            /> 
-            )}
+          {arrTask.map((e, index) => (
+            <ToDoitem id={index} delF={deleteItem} text={e} />
+          ))}
         </ul>
       </div>
     </div>
